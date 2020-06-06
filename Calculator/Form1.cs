026a0e4e571ece77fb6fc75ce4f8d6407ca6bf3d
Plumberg <hana.ryzhuk@gmail.com>
2020-06-05 20:05:26 -0400

    Add project files.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if(resultTextBox.Text == "0")
+            {
+                resultTextBox.Clear();
+            }
+            Button button = (Button)sender;
+            resultTextBox.Text += button.Text;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            resultTextBox.Clear();
+            resultTextBox.Text = "0";
+        }
+
+        private void actionButton_Click(object sender, EventArgs e)
+        {
+            resultTextBox.Clear();
+        }
+    }
+}
